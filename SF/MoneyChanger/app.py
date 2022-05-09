a959edfcce9ba2a9e52f80a0545b83d65427a997
GarborSerey <garborfersru@gmail.com>
2022-05-09 16:53:25 +0300

    Работает, сделать аппер на название валют, спрятать token

--- SF/MoneyChanger/app.py
+++ SF/MoneyChanger/app.py
@@ -4,7 +4,7 @@ import requests
 import telebot
 from datetime import datetime
 from config import currencies
-from utils import ConvertionException
+from utils import ConvertionException, CurrenciesConvertor
 
 TOKEN = '5385395435:AAHAJ43C14RngNzgEUcfuE_3K_n9rp5JsNI'
 
@@ -29,68 +29,14 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-    def get_ticker(name: str):
-        for currency in currencies:
-            if currencies[currency]['Name'] == name:
-                ticker = currency
-                return ticker
-        raise ConvertionException(f'Валюта {name} недоступна')
 
-    values = message.text.split()
     try:
-        if len(values) == 3:
-            quote, base, amount = values
-            quote_ticker = get_ticker(quote)
-            base_ticker = get_ticker(base)
-            amount = int(amount)
-
-        elif len(values) == 5:
-            quote = values[0] + ' ' + values[1]
-            base = values[2] + ' ' + values[3]
-            amount = int(values[-1])
-            quote_ticker = get_ticker(quote)
-            base_ticker = get_ticker(base)
-
-        elif len(values) == 4:
-            amount = int(values[-1])
-            name = values[0] + ' ' + values[1]
-            try:
-                quote_ticker = get_ticker(name)
-                quote = name
-                base = values[2]
-                base_ticker = get_ticker(values[2])
-            except ConvertionException:
-                name = values[1] + ' ' + values[2]
-                base = name
-                quote = values[0]
-                quote_ticker = get_ticker(values[0])
-                base_ticker = get_ticker(name)
-
-        else:
-            raise ConvertionException('Некорректный запрос!')
-
-    except ValueError:
-        raise ConvertionException(f'Количество задано некорректно ---> "{values[-1]}"')
-
-    r = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
-    central_bank = json.loads(r.content)
-    central_bank['Valute']['RUB'] = {
-        "ID": "",
-        "NumCode": "1",
-        "CharCode": "RU",
-        "Name": "Рубль",
-        "Value": 1,
-        "Nominal": 1
-    }
-
-    refresh_date = datetime.strptime(central_bank['Date'], '%Y-%m-%dT%H:%M:%S%z')
-    result = (central_bank['Valute'][quote_ticker]['Value'] * central_bank['Valute'][base_ticker]['Nominal']) / \
-             (central_bank['Valute'][base_ticker]['Value'] * central_bank['Valute'][quote_ticker]['Nominal']) * \
-             amount
-
-    text = f'{amount} {quote} перевести в {base}\n === {result} {base}\nПо курсу ЦБ РФ на {refresh_date.strftime("%d %b %Y time:%H:%M")}'
-
-    bot.send_message(message.chat.id, text)
-
+        text = CurrenciesConvertor.convert(message)
+    except ConvertionException as e:
+        bot.reply_to(message, f'user error: \n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'bot error: \n{e}')
+    else:
+        bot.send_message(message.chat.id, text)
 
 bot.polling(none_stop=True)
--- SF/MoneyChanger/utils.py
+++ SF/MoneyChanger/utils.py
@@ -1,4 +1,7 @@
 import telebot
+import requests
+import json
+from datetime import datetime
 from config import currencies
 
 class ConvertionException(Exception):
@@ -6,8 +9,8 @@ class ConvertionException(Exception):
 
 
 class CurrenciesConvertor:
-    @staticmethod
-    def split_message(message: telebot.types.Message):
+
+    def handler_message(message: telebot.types.Message):
 
         def get_ticker(name: str):
             for currency in currencies:
@@ -15,8 +18,79 @@ class CurrenciesConvertor:
                     ticker = currency
                     return ticker
             raise ConvertionException(f'Валюта {name} недоступна')
-        pass
+
+        values = message.text.split()
+        try:
+            if len(values) == 3:
+                quote, base, amount = values
+                quote_ticker = get_ticker(quote)
+                base_ticker = get_ticker(base)
+                amount = int(amount)
+
+            elif len(values) == 5:
+                quote = values[0] + ' ' + values[1]
+                base = values[2] + ' ' + values[3]
+                amount = int(values[-1])
+                quote_ticker = get_ticker(quote)
+                base_ticker = get_ticker(base)
+
+            elif len(values) == 4:
+                amount = int(values[-1])
+                name = values[0] + ' ' + values[1]
+                try:
+                    quote_ticker = get_ticker(name)
+                    quote = name
+                    base = values[2]
+                    base_ticker = get_ticker(values[2])
+                except ConvertionException:
+                    name = values[1] + ' ' + values[2]
+                    base = name
+                    quote = values[0]
+                    quote_ticker = get_ticker(values[0])
+                    base_ticker = get_ticker(name)
+
+            else:
+                raise ConvertionException('Некорректный запрос!')
+
+        except ValueError:
+            raise ConvertionException(f'Количество задано некорректно ---> "{values[-1]}"')
+
+        result = {
+            'quote': quote,
+            'quote_ticker': quote_ticker,
+            'base': base,
+            'base_ticker': base_ticker,
+            'amount': amount,
+        }
+
+        return result
 
     @staticmethod
-    def convert():
-        pass
+    def convert(message: telebot.types.Message):
+        d = CurrenciesConvertor.handler_message(message)
+
+        if d['quote'] == d['base']:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты {d["base"]}')
+
+        r = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+        central_bank = json.loads(r.content)
+        central_bank['Valute']['RUB'] = {
+                "ID": "1",
+                "NumCode": "1",
+                "CharCode": "RU",
+                "Name": "Рубль",
+                "Value": 1,
+                "Nominal": 1
+            }
+
+        refresh_date = datetime.strptime(central_bank['Date'], '%Y-%m-%dT%H:%M:%S%z')
+        result = (central_bank['Valute'][d['quote_ticker']]['Value'] * central_bank['Valute'][d['base_ticker']]['Nominal']) / \
+                 (central_bank['Valute'][d['base_ticker']]['Value'] * central_bank['Valute'][d['quote_ticker']]['Nominal']) * \
+                 d['amount']
+
+        text = f"{d['amount']} {d['quote']} перевести в {d['base']}\n === {result} {d['base']}\n" \
+               f"По курсу ЦБ РФ на {refresh_date.strftime('%d %b %Y time:%H:%M')}"
+
+        return text
+
+
